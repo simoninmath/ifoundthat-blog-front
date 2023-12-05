0322643112e6c259a3607edf0c6183ff02e3e3d7
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-05 17:51:48 +0100

    [comp]-create-newsletter-comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
