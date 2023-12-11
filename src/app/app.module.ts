f634d5e25437ce4a5f4eebdd8fb936a31baf8488
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-11 16:37:00 +0100

    [setup]-clear-all-files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsletterComponent
   ],
   imports: [
     BrowserModule,
