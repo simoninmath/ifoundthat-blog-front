b8a11c8597dd71bcfd66beb99116c0a308ca880a
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-12 15:54:28 +0100

    [routes]-update-mainroutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './404/page-not-found.component';
 
-const routes: Routes = [
-  { path:'', redirectTo: 'figurine', pathMatch: 'full' },  // Defautl Route with pathMath to avoid "side effects"
+const mainRoutes: Routes = [
+  { path:'', redirectTo: 'home', pathMatch: 'full' },  // Defautl Route with pathMath to avoid "side effects"
   { path:'**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(mainRoutes)],
   exports: [RouterModule]
 })
 
