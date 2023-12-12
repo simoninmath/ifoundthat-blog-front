1027b0bc3029248e77db4bd66f8a05c659fc4cc1
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-12 14:48:32 +0100

    [module]-create-profil-module

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
+import { ProfilModule } from './profil/profil.module';
 
 const protectedRoutes: Routes = [
   { path: 'protected'}
@@ -9,7 +10,8 @@ const protectedRoutes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ProfilModule
   ]
 })
 
