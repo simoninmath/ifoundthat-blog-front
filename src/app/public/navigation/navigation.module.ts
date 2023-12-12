bc108556d231997285f0634c865cda026a723f42
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-12 16:23:20 +0100

    [comp]-create-searchbar

--- src/app/public/navigation/navigation.module.ts
+++ src/app/public/navigation/navigation.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavSmartComponent } from './nav-smart/nav-smart.component';
+import { SearchSmartComponent } from './search-smart/search-smart.component';
 
 
 
 @NgModule({
   declarations: [
-    NavSmartComponent
+    NavSmartComponent,
+    SearchSmartComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/public/navigation/search-smart/search-smart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-smart',
+  templateUrl: './search-smart.component.html',
+  styleUrls: ['./search-smart.component.scss']
+})
+export class SearchSmartComponent {
+
+}
