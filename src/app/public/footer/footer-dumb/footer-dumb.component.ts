d62d55a61d30b24fb637b4ca9a4f578597f17e65
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-12 16:10:01 +0100

    [comp]-create-footer

---
+++ src/app/public/footer/footer-dumb/footer-dumb.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-dumb',
+  templateUrl: './footer-dumb.component.html',
+  styleUrls: ['./footer-dumb.component.scss']
+})
+export class FooterDumbComponent {
+
+}
--- src/app/public/footer/footer.module.ts
+++ src/app/public/footer/footer.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterDumbComponent } from './footer-dumb/footer-dumb.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterDumbComponent
+  ],
   imports: [
     CommonModule
   ]
