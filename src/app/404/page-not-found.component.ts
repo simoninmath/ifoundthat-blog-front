3ca64d30b3abcb36a10293e38e9df3e98039acae
SIMONIN Mathieu <simonin.math@gmail.com>
2023-12-28 21:27:21 +0100

    [view]-create-404-page

--- src/app/404/page-not-found.component.ts
+++ src/app/404/page-not-found.component.ts
@@ -1,8 +1,15 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'app-page-not-found',
-    template: `<h1>404 ERROR</h1>`
+  selector: "app-page-not-found",
+  template: `
+    <div class="center">
+      <h1>404 error! Page not found</h1>
+      <a routerLink="/home" class="waves-effect waves-light btn blue-grey darken-2">Home</a>
+      <div class="center">
+        <img src="" />
+      </div>
+    </div>
+  `,
 })
-
 export class PageNotFoundComponent {}
